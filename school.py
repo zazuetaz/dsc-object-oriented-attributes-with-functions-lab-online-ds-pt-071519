a4593b106b6ddd1b9b789f25fec15c2f616600dd
Zach Zazueta <zazuetaz2103@gmail.com>
2019-09-16 19:21:32 -0400

    tricky

--- school.py
+++ school.py
@@ -1,1 +1,16 @@
 class School:
+    def __init__(self, name):
+        self.name = name
+        self.roster = {} 
+    def add_student(self, name, grade):
+        if grade in self.roster:
+            self.roster[grade].append(name)
+        else:
+            self.roster[grade] = [name]
+    def grade(self, grade):
+        return self.roster[grade]
+    def sort_roster(self):
+        new_dict = self.roster
+        for key in new_dict:
+            new_dict[key].sort()
+        return new_dict
